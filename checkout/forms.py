0fc8e6f8fdd2fabe95dd072048b44ce074a57bf0
CathalShandon <Cathalshandon@gmail.com>
2022-10-17 11:16:22 +0000

    added stripe elements

--- checkout/forms.py
+++ checkout/forms.py
@@ -6,9 +6,9 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
-                    'street_address1', 'street_address2',
-                    'town_or_city', 'postcode', 'country',
-                    'county',)
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
 
     def __init__(self, *args, **kwargs):
         """
@@ -27,6 +27,7 @@ class OrderForm(forms.ModelForm):
             'street_address2': 'Street Address 2',
             'county': 'County',
         }
+
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if self.fields[field].required:
--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+
 from .forms import OrderForm
 
 
@@ -7,12 +8,14 @@ def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Theres nothing in your bag at the moment")
-        return redirect(reversed('products'))
+        return redirect(reverse('products'))
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_from': order_form,
+        'order_form': order_form,
+        'stripe_public_views': 'pk_test_51LtqTjAEmywH3arzDLDb85w2wkCjaoe3QMQNSZG7snWv2AJ1GGsAk6ogGejpt1uCMzBhd2QyA1I89RU9c4xGifXD00FzhIryh5',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
